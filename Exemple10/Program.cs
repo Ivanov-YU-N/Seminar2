6c35699a16b6c823ec171e06277925ccaa46a3cf
New <New@example.com>
2023-02-15 18:41:17 +0500

    Пример № 10

---
+++ Exemple10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int Exemple(int number)
+{
+    int resalt = (number / 10) % 10;
+    return resalt;
+}
+
+int number = new Random().Next(1, 1000);
+Console.Write("Трехзначное число = ");
+Console.WriteLine(number);
+Console.Write("Вторая циффра числа = ");
+Console.WriteLine(Exemple(number));
